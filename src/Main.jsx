311e6f07911ef4f46a62c4e4ad37f47ed9c7f7d8
elkake <gianco.marquez@gmail.com>
2023-03-11 11:44:43 -0300

    probando

--- src/Main.jsx
+++ src/Main.jsx
@@ -5,7 +5,6 @@ import App from './App'
 import './style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
     <React.StrictMode>
         <App/>
     </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,10 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  jsx: {
+    factory: 'React.createElement',
+    fragment: 'React.Fragment'
+  }
+  // otras opciones de configuración de Vite aquí
 })
